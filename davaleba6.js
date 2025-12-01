87e2be9445639bf881e2568cae12a4c39831278f
Lukanikolaishvili0 <lukanikola10@gmail.com>
2025-12-02 00:50:31 +0400

    davaleba11.js

---
+++ davaleba6.js
@@ -0,0 +1,5 @@
+//6. დაწერეთ მასივი fruits ელემენტებით ["Apple", "Banana", "Orange"]; დაბეჭდეთ მასივის თითოეული წევრი.
+const fruits=["apple","banana","Orange"];
+for(let fruit of fruits) {
+    console.log(fruits);
+} 
---
+++ davaleba8.js
@@ -0,0 +1,5 @@
+//დაბეჭდეთ fruits მასივის ყოველი მეორე ელემენტი და მისი ინდექსი
+const fruits=["apple","banana","Orange"];
+for(let i=0; i < fruits.length; i +=2){
+    console.log(i,fruits[i]);
+}
